af047e161791a7446144506c2dd21e757a709c36
amankansal <aman.hobo@gmail.com>
2024-07-28 19:40:48 +0530

    Add Cors

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
+use rocket::serde::json::{self, Json};
+use rocket::serde::json::{json, Value};
+use rocket::http::Method;
+use rocket_cors::{AllowedOrigins, CorsOptions};
+
 #[macro_use] extern crate rocket;
-#[allow(unused_imports)]
-use rocket::Data;
 
 #[get("/")]
-async fn index() -> String{
-    String::from("Hello World")
+fn index() -> String{
+    String::from("Hello World!!")
+}
+
+#[get("/name")]
+fn name() -> Value{
+  json!({
+    "name": "Aman kansal",
+    "id": 56
+  })
 }
 
 #[launch]
-async fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+fn rocket() -> _ {
+    let cors = CorsOptions::default()
+    .allowed_origins(AllowedOrigins::all())
+    .allowed_methods(
+        vec![Method::Get, Method::Post, Method::Patch]
+            .into_iter()
+            .map(From::from)
+            .collect(),
+    )
+    .allow_credentials(true);
+
+    rocket::build()
+    .attach(cors.to_cors().unwrap())
+    .mount("/", routes![index, name])
 }
