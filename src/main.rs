ea2e3ffabc79e9fc2902ea4ccae0a04bc7bfaf06
amankansal <aman.hobo@gmail.com>
2024-08-05 02:36:41 +0530

    create post route

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-use rocket::serde::json::{self, Json};
+#[allow(unused_imports)]
+use rocket::serde::json::Json;
 use rocket::serde::json::{json, Value};
 use rocket::http::Method;
 use rocket_cors::{AllowedOrigins, CorsOptions};
+use serde::{Deserialize, Serialize};
+
 
 #[macro_use] extern crate rocket;
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
+struct User{
+  id: u32,
+  name: String,
+  balance: u32,
+  above_18: bool
+}
+
+static mut DATA: Vec<User> = Vec::new();
 
 #[get("/")]
 fn index() -> String{
@@ -12,10 +25,24 @@ fn index() -> String{
 
 #[get("/name")]
 fn name() -> Value{
+  let name: &str = "Aman Kansal";
   json!({
-    "name": "Aman kansal",
+    "name": name,
     "id": 56
   })
+}
+
+#[post("/user", format="json", data="<input>")]
+fn user(input: Json<User>) -> (){
+  let temp = input.into_inner();
+  unsafe{DATA.push(temp);}
+}
+
+#[get("/userdata")]
+fn userdata() -> Value{
+  unsafe{
+    json!({"data": DATA})
+  }
 }
 
 #[launch]
@@ -23,7 +50,7 @@ fn rocket() -> _ {
     let cors = CorsOptions::default()
     .allowed_origins(AllowedOrigins::all())
     .allowed_methods(
-        vec![Method::Get, Method::Post, Method::Patch]
+        vec![Method::Get, Method::Post, Method::Patch, Method::Delete, Method::Options]
             .into_iter()
             .map(From::from)
             .collect(),
@@ -32,5 +59,7 @@ fn rocket() -> _ {
 
     rocket::build()
     .attach(cors.to_cors().unwrap())
-    .mount("/", routes![index, name])
+    .mount("/", routes![index])
+    .mount("/", routes![name])
+    .mount("/name", routes![user, userdata])
 }
